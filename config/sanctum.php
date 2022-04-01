f7018fb6c520b44e9147625c9dd227e289be373d
WyomingCPA@yandex.ru <WyomingCPA@yandex.ru>
2022-04-01 21:27:56 +0300

    shift new domain

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'techniquef.ani24.fun,localhost,localhost:8080,127.0.0.1,127.0.0.1:8000,::1',
+        'techniquef.simpleitrunner.ru,localhost,localhost:8080,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
