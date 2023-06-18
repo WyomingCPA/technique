66f6df6712a867ddb16d6e03e72c0a4f8ca10b28
Nikolay <WyomingCPA@yandex.ru>
2023-06-18 21:01:25 +0300

    work with washmashine table

--- admin/src/pages/admin/products/washmasine.vue
+++ admin/src/pages/admin/products/washmasine.vue
@@ -1,159 +1,60 @@
 <template>
-  <section class="tables">
-    <div class="page-header">
-      <h3 class="page-title">Продукты</h3>
-      <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="javascript:void(0);">Table</a>
-          </li>
-          <li class="breadcrumb-item active" aria-current="page">Все группы</li>
-        </ol>
-      </nav>
-    </div>
-    <div class="row">
-      <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <h4 class="card-title"></h4>
-                        <b-row>
-              <div class="col-sm-7">
-                <b-form-group
-                  label="Filter"
-                  label-for="filter-input"
-                  label-cols-sm="3"
-                  label-align-sm="right"
-                  label-size="sm"
-                  class="mb-0"
-                >
-                  <b-input-group size="sm">
-                    <b-form-input
-                      id="filter-input"
-                      v-model="filter"
-                      type="search"
-                      placeholder="Type to Search"
-                    ></b-form-input>
+  <div class="grid md:grid-cols-2 gap-6 mb-6">
+    <va-input v-model="input" placeholder="Filter..." class="w-full" />
 
-                    <b-input-group-append>
-                      <b-button :disabled="!filter" @click="filter = ''"
-                        >Clear</b-button
-                      >
-                    </b-input-group-append>
-                  </b-input-group>
-                </b-form-group>
-              </div>
-            </b-row>
-            <b-row>
-              <div class="col-sm-12">
-                <span v-for="(thing, index) in listPrice" :key="index"
-                  ><a href="#" @click="filterPrice(index)">
-                    {{ index }}({{ thing }})
-                  </a></span
-                >
-              </div>
-            </b-row>
-            <b-table
-              responsive
-              :busy="loading"
-              :items="items"
-              :fields="fields"
-              :sort-by.sync="sortBy"
-              :sort-desc.sync="sortDesc"
-              :filter="filter"
-              :filter-included-fields="filterOn"
-              show-empty
-              small
-            >
-              <template #table-busy>
-                <div class="text-center text-danger my-2">
-                  <b-spinner class="align-middle"></b-spinner>
-                  <strong>Loading...</strong>
-                </div>
-              </template>
-              <template #cell(actions)="row">
-                <b-button
-                  size="sm"
-                  @click="setLearn(row.item, row.index, $event.target)"
-                  class="mr-1"
-                >
-                  Повторил
-                </b-button>
-              </template>
-              <template v-slot:cell(thumbnail)="data">
-                <a
-                  :href="'https://www.techprom.ru' + data.item.link"
-                  target="_blank"
-                  ><img
-                    :src="'https://www.techprom.ru' + data.value"
-                    class="img-thumbnail rounded-0"
-                /></a>
-              </template>
-            </b-table>
-          </div>
-        </div>
-      </div>
+    <div class="flex flex-col gap-6">
+      <va-checkbox v-model="isCustomFilteringFn" label="Use custom filtering function (looks for an exact match)" />
+      <va-checkbox v-model="isDebounceInput" label="Debounce input" />
     </div>
-  </section>
+  </div>
+
+  <va-data-table :items="items" :columns="columns" :filter="filter" :filter-method="customFilteringFn"
+    @filtered="filteredCount = $event.items.length" />
+
+  <va-alert class="!mt-6" color="info" outline>
+    Number of filtered items:
+    <va-chip>{{ filteredCount }}</va-chip>
+  </va-alert>
 </template>
 <script>
 import axios from "axios";
+import debounce from "lodash/debounce.js";
 
 export default {
   name: "product-all",
   components: {},
   data() {
+    
+    const columns = [
+      { key: "id", sortable: true },
+      { key: "username", sortable: true },
+      { key: "name", sortable: true },
+      { key: "email", sortable: true },
+      { key: "address.zipcode", label: "Zipcode" },
+    ];
     return {
-      filter: null,
-      filterOn: [],
-      listPrice: Array,
-      loading: false,
-      sortBy: "count_learn",
-      sortDesc: false,
-      countSubscriber: 0,
-      idGroup: "",
-      idProduct: "",
       items: Array,
-      infoModal: {
-        id: "info-modal",
-        title: "",
-        content: "",
-      },
-      fields: [
-        {
-          key: "thumbnail",
-          label: "img",
-          //formatter(value) {
-          //  return `https://www.techprom.ru${value}`;
-          //},
-        },
-        {
-          key: "name",
-          label: "Название",
-          sortable: true,
-        },
-        {
-          key: "count_learn",
-          label: "Количество повторений",
-          sortable: true,
-        },
-        {
-          key: "price",
-          label: "Цена",
-          sortable: true,
-        },
-        {
-          key: "updated_at",
-          label: "Последнее обновление",
-          sortable: true,
-          // Variant applies to the whole column, including the header and footer
-        },
-        { key: "actions", label: "Actions" },
-      ],
+      columns,
     };
   },
   methods: {
+    filterExact(source) {
+      if (this.filter === "") {
+        return true;
+      }
+      return source?.toString?.() === this.filter;
+    },
+
+    updateFilter(filter) {
+      this.filter = filter;
+    },
+
+    debouncedUpdateFilter: debounce(function (filter) {
+      this.updateFilter(filter);
+    }, 600),
+
     filterPrice: function (param) {
-      this.filter=param;
+      this.filter = param;
     },
     setLearn(item, index, button) {
       this.loading = true;
