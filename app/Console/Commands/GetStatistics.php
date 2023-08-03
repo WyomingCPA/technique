150e2bb70a5a4166420a258283e91d2e9b5110cb
Nikolay <WyomingCPA@yandex.ru>
2023-08-03 20:37:55 +0300

    work with command

--- app/Console/Commands/GetStatistics.php
+++ app/Console/Commands/GetStatistics.php
@@ -3,7 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\User;
 
+use App\Models\Product;
+use App\Models\Statistics;
 class GetStatistics extends Command
 {
     /**
@@ -11,14 +14,14 @@ class GetStatistics extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'command:get-statistics';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Get Data every day from bd';
 
     /**
      * Create a new command instance.
@@ -37,6 +40,13 @@ class GetStatistics extends Command
      */
     public function handle()
     {
+        $total_all_product = Product::all()->count();
+        $count_available_product = Product::where('status', true)->where('city', 'kor')->orderBy('price', 'desc')->get()->count();
+        $user = User::select('id')->where('email', 'WyomingCPA@yandex.ru')->first();
+        $total_favorite_product = $user->favoritesProduct()->where('status', true)->get()->count();
+        $count_all_research_product = Product::where('status', true)->where('city', 'kor')->orderBy('price', 'desc')->get()->sum('count_learn');
+        echo $count_all_research_product . "\n";
+
         return 0;
     }
 }
